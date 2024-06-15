d48873761ef06fc35e49004ecff57cd001ee9110
daulet <dauka2707@gmail.com>
2024-06-15 17:57:33 +0600

    prisma

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const uploadDestination = 'uploads';
+
+const storage = multer.diskStorage({
+    destination:
+})
 
 router.get('/register', (req, res) => {
     res.send('/register: works!')
